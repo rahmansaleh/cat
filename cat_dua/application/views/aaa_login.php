73be29335658aea151f854fef3c65229b6875e24
Muhammad Rahman Saleh <36500308+rahmansaleh@users.noreply.github.com>
2020-11-20 18:34:04 +0700

    customize login page

--- cat_dua/application/views/aaa_login.php
+++ cat_dua/application/views/aaa_login.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 <meta charset="utf-8">
-<title>Aplikasi Ujian Online</title>
+<title>RSUD Tanah Abang - CAT</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 <meta name="description" content="Aplikasi Ujian Online berbasis web. Lebih Mudah dan Hemat, kayak AlfaMart">
 <meta name="apple-mobile-web-app-capable" content="yes">
@@ -16,12 +16,17 @@
 	<div class="col-md-4">
 	<form action="" method="post" name="fl" id="f_login" onsubmit="return login();">
 		
+
 		<div class="panel panel-default top150">
-			<div class="panel-heading"><h4 style="margin: 5px"><i class="glyphicon glyphicon-user"></i> Login Aplikasi</h4></div>
+			<div class="panel-heading text-center panel-relative">
+			<img src="<?php echo base_url(); ?>assets/img/rsudta-64.png" width="140px">
+				<br>
+				<h4 style="margin: 5px; margin-top: 20px">Aplikasi Digital Computer Assisted Test RSUD Tanah Abang</h4>
+			</div>
 			<div class="panel-body">
 				<div id="konfirmasi"></div>
 				<div class="input-group">
-					<span class="input-group-addon">@</span>
+					<span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
 					<input type="text" id="username" name="username" autofocus value="" placeholder="Username" class="form-control" />
 				</div> <!-- /field -->
 				
@@ -30,7 +35,7 @@
 					<input type="password" id="password" name="password" value="" placeholder="Password" class="form-control"/>
 				</div> <!-- /password -->
 				<div class="login-actions">
-					<button class="button btn btn-dafault btn-large col-lg-12 top15">Login</button>
+					<button class="button btn btn-primary btn-large col-lg-12 top15">Login</button>
 				</div> <!-- .actions -->
 			</div>
 		</div> <!-- /login-fields -->
@@ -42,7 +47,7 @@
 </div> 
 
 <div class="ctr"> 
-	<a href="<?php echo base_url(); ?>adm"><?php echo $this->config->item('nama_aplikasi')." ".$this->config->item('versi'); ?></a>
+	<!-- <a href="<?php echo base_url(); ?>adm"><?php echo $this->config->item('nama_aplikasi')." ".$this->config->item('versi'); ?></a> -->
 </div>
 
 <script src="<?php echo base_url(); ?>___/js/jquery-1.11.3.min.js"></script> 

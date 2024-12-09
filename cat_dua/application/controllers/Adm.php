d952b1e8d531f15a1ddce182f057a13e3c1746d0
simrsta <simrs.rsudta@gmail.com>
2024-12-09 12:08:46 +0700

    fix bug

--- cat_dua/application/controllers/Adm.php
+++ cat_dua/application/controllers/Adm.php
@@ -1,8 +1,13 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php 
+defined('BASEPATH') or exit('No direct script access allowed');
+
 date_default_timezone_set("Asia/Jakarta");
+
 class Adm extends CI_Controller {
+
 	function __construct() {
         parent::__construct();
+		$this->load->helper('url');
         $this->db->query("SET time_zone='+7:00'");
         $waktu_sql = $this->db->query("SELECT NOW() AS waktu")->row_array();
         $this->waktu_sql = $waktu_sql['waktu'];
